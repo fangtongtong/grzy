bc636eeb618af5d8449c61dbc38f11293c38d767
fangtongtong <1370791265@qq.com>
2017-01-14 18:33:49 +0800

    add

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+$(function(){
+    $(".enter p").click(function(){
+        $(".nav").css({
+            opacity:1
+        })
+        $(".welcome").css({
+            opacity:0
+        })
+
+    })
+})
+
